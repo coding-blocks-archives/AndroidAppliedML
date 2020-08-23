b328e8d92746feed360bf0f51db47c3ac698e2ab
Arnav Gupta <arnav@codingblocks.com>
2020-08-23 08:56:49 +0530

    get thumbnail from external camera
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- TakePhotoBasics/app/src/main/java/com/codingblocks/takephotobasics/MainActivity.kt
+++ TakePhotoBasics/app/src/main/java/com/codingblocks/takephotobasics/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.codingblocks.takephotobasics
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        @JvmStatic val PHOTO_REQ_CODE = 234
+        @JvmStatic val EXTRA_DATA = "data"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnTakePhoto.setOnClickListener {
+            val takePhotoIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(takePhotoIntent, PHOTO_REQ_CODE)
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (requestCode == PHOTO_REQ_CODE) {
+            val bitmap = data?.extras?.get(EXTRA_DATA) as Bitmap
+            imgThumb.setImageBitmap(bitmap)
+            return
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
