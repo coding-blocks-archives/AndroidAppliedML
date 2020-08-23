95d5b4f2d3dacf103662fd23d33a328799ecfce3
Arnav Gupta <arnav@codingblocks.com>
2020-08-23 11:31:17 +0530

    cameraX part
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- TakePhotoBasics/app/src/main/java/com/codingblocks/takephotobasics/MainActivity.kt
+++ TakePhotoBasics/app/src/main/java/com/codingblocks/takephotobasics/MainActivity.kt
@@ -1,27 +1,39 @@
 package com.codingblocks.takephotobasics
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     companion object {
-        @JvmStatic val PHOTO_REQ_CODE = 234
-        @JvmStatic val EXTRA_DATA = "data"
+        @JvmStatic
+        val PHOTO_REQ_CODE = 234
+        @JvmStatic
+        val EXTRA_DATA = "data"
     }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnTakePhoto.setOnClickListener {
+        btnTakeExtPhoto.setOnClickListener {
             val takePhotoIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             startActivityForResult(takePhotoIntent, PHOTO_REQ_CODE)
         }
+
+        btnCameraActivity.setOnClickListener {
+            startActivity(Intent(this, CameraActivity::class.java))
+        }
     }
+
 
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
